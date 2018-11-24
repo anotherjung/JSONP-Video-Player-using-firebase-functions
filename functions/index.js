1ef8059b2793c367c9933e27eed27568b912efad
Jung No <jungno@gmail.com>
2018-11-24 17:17:38 -0500

    adding firebase functions to handle jsonp

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const http = require('requestify');
+const cors = require('cors')({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.franklyProxy = functions.https.onRequest((req, res) => {
+    console.log(1, 'franklyProxy ')
+    cors (req, res, () => {
+        playlist = 'https://s3.amazonaws.com/frankly-news-web/test/playlist.js'
+
+        return http.get(playlist).then( response => {
+            function callback(obj) { return obj }
+            var parsed = eval( response.getBody() );
+
+            console.log(2, 'playlistbody2 ', parsed);
+            //return res.status(200).send(JSON.stringify(parsed));
+            return res.status(200).send(parsed);
+        })
+        .catch(err => {
+            console.log(3,'frankly err',err)
+            return res.status(400).send(err) 
+        }) 
+    })
+});
